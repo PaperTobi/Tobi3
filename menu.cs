8f3ce43551488b6938559bdddcf13e9db70ddb6f
Tobias <73227066+PaperTobi@users.noreply.github.com>
2024-04-22 06:13:10 +0200

    menu

--- menu.cs
+++ menu.cs
@@ -3,27 +3,9 @@ using System;
 
 public partial class menu : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-	// 
 	private void _on_level1_pressed()
 	{
-		// Replace with function body.
 		GetTree().ChangeSceneToFile("res://level/level.tscn");
-	}
-	//
-	private void _on_level2_pressed()
-	{
-		// Replace with function body.
-
 	}
 	private void _on_level_2_pressed()
 	{
@@ -31,12 +13,6 @@ public partial class menu : Control
 	}
 	private void _on_exit_pressed()
 	{
-		// Replace with function body.
-		GD.Print("Exit button");
 		GetTree().Quit();
 	}
-
 }
-
-
-
