1d009c08815793839d2e1addf791b3ceb14a5e33
Tobias <73227066+PaperTobi@users.noreply.github.com>
2024-05-02 15:43:07 +0200

    removed build

--- build/Linux/0.2/Tobi3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo -ne '\033c\033]0;Tobi3\a'
-base_path="$(dirname "$(realpath "$0")")"
-"$base_path/Tobi3.x86_64" "$@"
--- build/Linux/2DJ&R.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo -ne '\033c\033]0;2DJ&R\a'
-base_path="$(dirname "$(realpath "$0")")"
-"$base_path/2DJ&R.x86_64" "$@"
